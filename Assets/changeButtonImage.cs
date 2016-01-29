ed04f76145142145ab8c9c3527b027946f758b3b
msLangdon95 <sameerasalameh95@gmail.com>
2016-01-29 10:37:10 -0800

    version0

---
+++ Assets/changeButtonImage.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class changeButtonImage : MonoBehaviour {
+	public Sprite OtherSprite;
+	public GameObject originalImage;
+	Image[] images;
+
+	public void swapImage()
+	{
+		images = originalImage.gameObject.GetComponentsInChildren<Image>();
+		foreach (Image image in images)
+		{
+			image.sprite = OtherSprite;
+		}
+	}
+
+}
---
+++ Assets/newscene.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+public class newscene : MonoBehaviour 
+{
+	public void NextLevelButton(int index)
+	{
+		Application.LoadLevel(index);
+	}
+	
+	public void NextLevelButton(string levelName)
+	{
+		Application.LoadLevel(levelName);
+	}
+}

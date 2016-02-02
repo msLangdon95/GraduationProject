40cefd0970e2b9518aeadf685548cd9a65ae3e91
msLangdon95 <sameerasalameh95@gmail.com>
2016-02-02 02:16:06 -0800

    rotate around y axis

--- Assets/RotateWhole.cs
+++ Assets/RotateWhole.cs
@@ -4,7 +4,9 @@ using UnityEngine;
  public class RotateWhole : MonoBehaviour {
  
      float f_lastX = 0.0f;
+	 float f_lastY = 0.0f;
      float f_difX = 0.5f;
+	 float f_difY=0.5f;
      float f_steps = 0.0f;
      int i_direction = 1;
  
@@ -20,29 +22,42 @@ using UnityEngine;
          if (Input.GetMouseButtonDown(0))
          {
              f_difX = 0.0f;
+			 f_difY=0.0f;
          }
          else if (Input.GetMouseButton(0))
          {
              f_difX = Mathf.Abs(f_lastX - Input.GetAxis ("Mouse X"));
- 
+			 f_difY= Mathf.Abs(f_lastY - Input.GetAxis ("Mouse Y"));
              if (f_lastX < Input.GetAxis ("Mouse X"))
              {
                  i_direction = -1;
                  transform.Rotate(Vector3.up, -f_difX);
              }
- 
+			 if (f_lastY < Input.GetAxis ("Mouse Y"))
+             {
+                 i_direction = -1;
+                 transform.Rotate(Vector3.left, -f_difY);
+             }
              if (f_lastX > Input.GetAxis ("Mouse X"))
              {
                  i_direction = 1;
                  transform.Rotate(Vector3.up, f_difX);
              }
+			 if (f_lastY > Input.GetAxis ("Mouse Y"))
+             {
+                 i_direction = 1;
+                 transform.Rotate(Vector3.left, f_difY);
+             }
  
              f_lastX = -Input.GetAxis ("Mouse X");
+			 f_lastY = -Input.GetAxis ("Mouse Y");
          }
-        /* else 
+        /*else 
          {
              if (f_difX > 0.5f) f_difX -= 0.05f;
+			 if (f_difY > 0.5f) f_difY -= 0.05f;
              if (f_difX < 0.5f) f_difX += 0.05f;
+			 if (f_difY < 0.5f) f_difY += 0.05f;
  
              transform.Rotate(Vector3.up, f_difX * i_direction);
          }*/

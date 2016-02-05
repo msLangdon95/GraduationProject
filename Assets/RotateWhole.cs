db14b1d581911bf2509c4067a6e1990a0edeb7c7
msLangdon95 <sameerasalameh95@gmail.com>
2016-02-05 09:22:15 -0800

    touch rotation, not ready yet

--- Assets/RotateWhole.cs
+++ Assets/RotateWhole.cs
@@ -3,40 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 
  public class RotateWhole : MonoBehaviour {
-	/* public GameObject target;// var target : Transform;
-	 float edgeBorder = 0.1f;
-	 float horizontalSpeed = 360.0f;
-	 float verticalSpeed = 120.0f;
-	 float minVertical = 20.0f;
-	 float maxVertical = 85.0f;
-	 float  x = 0.0f;
-	 float y = 0.0f;
-	 float  distance = 0.0f;
-	 
-	 void Start(){
-		 x = transform.eulerAngles.y;
-		 y = transform.eulerAngles.x;
-		 distance = Camera.mainCamera.gameObject.transform.position - target.transform.position;
-	 }
-	 void Update(){
-	 }*/
-	 
-	 
-	 
-	/*  public GameObject target;//the target object
-		private float speedMod = 10.0f;//a speed modifier
-		private Vector3 point;//the coord to the point where the camera looks at
-   
-    void Start () {//Set up things on the start method
-        point = target.transform.position;//get target's coords
-        transform.LookAt(point);//makes the camera look to it
-    }
-   
-    void Update () {//makes the camera rotate around "point" coords, rotating around its Y axis, 20 degrees per second times the speed modifier
-        transform.RotateAround (point,new Vector3(0.0f,1.0f,0.0f),20 * Time.deltaTime * speedMod);
-    }
- */
-    /* float f_lastX = 0.0f;
+     float f_lastX = 0.0f;
 	 float f_lastY = 0.0f;
      float f_difX = 0.5f;
 	 float f_difY=0.5f;
@@ -54,39 +21,27 @@ using System.Collections.Generic;
          if (Input.GetMouseButtonDown(0))
          {
              f_difX = 0.0f;
-	     f_difY=0.0f;
+			 f_difY=0.0f;
          }
          else if (Input.GetMouseButton(0))
          {
              f_difX = Mathf.Abs(f_lastX - Input.GetAxis ("Mouse X"));
-	     f_difY= Mathf.Abs(f_lastY - Input.GetAxis ("Mouse Y"));
+			 f_difY= Mathf.Abs(f_lastY - Input.GetAxis ("Mouse Y"));
              if (f_lastX < Input.GetAxis ("Mouse X")){
-		     transform.RotateAround (point, Vector3.up, 1);
+				transform.RotateAround (point, Vector3.up, 1);
              }
 	     if (f_lastX > Input.GetAxis ("Mouse X")){
                 transform.RotateAround (point, Vector3.up, -1);
              }
-			 /*
 			 if (f_lastY < Input.GetAxis ("Mouse Y")){
                  transform.Rotate(Vector3.back, f_difY);
-             }
-			/* if (f_lastZ < Input.GetAxis ("Mouse ScrollWheel"))
-             {
-                 transform.Rotate(Vector3.up, -f_difZ);
-             }
-			 */	 
-             
-			/* if (f_lastY > Input.GetAxis ("Mouse Y")){
+             } 
+			 if (f_lastY > Input.GetAxis ("Mouse Y")){
                  transform.Rotate(Vector3.left, f_difY);
-             }*/
-			 /*if (f_lastZ > Input.GetAxis ("Mouse ScrollWheel"))
-             {
-                 i_direction = 1;
-                 transform.Rotate(Vector3.left, f_difZ);
              }
  
              f_lastX = -Input.GetAxis ("Mouse X");
 			 f_lastY = -Input.GetAxis ("Mouse Y");
          }
-     }*/
+     }
  }
--- Assets/Try.cs
+++ Assets/Try.cs
@@ -8,13 +8,9 @@
      public float ySpeed = 120.0f;
      public float yMinLimit = 20f;
      public float yMaxLimit = 85f;
-	 
-	/* public float xMinLimit = 20f;
-     public float xMaxLimit = 85f;*/
-	 
      private float x = 0.0f;
      private float y = 0.0f;
-	 
+
      void Start(){
          var angles = transform.eulerAngles;
          x = angles.y;
@@ -23,22 +19,20 @@
      }
      
      void Update(){
-			 var dt = Time.deltaTime;
-             x += Input.GetAxis("Horizontal") * xSpeed * dt;
-             y -= Input.GetAxis("Vertical") * ySpeed * dt;
-             
-             y = ClampAngle(y, yMinLimit, yMaxLimit);
-            // x = ClampAngle(x, xMinLimit, xMaxLimit);
-			 
-             var rotation = Quaternion.Euler(y, x, 0f);
-             var position = rotation * new Vector3(0.0f, 0.0f, -distance) + target.position;
-             
-             transform.rotation = rotation;
-             transform.position = position;
-     }
+		 var dt = Time.deltaTime;
+		 if (Input.GetMouseButton(0)){
+			 x += Input.GetAxis("Mouse X") * xSpeed * dt;
+			 y -= Input.GetAxis("Mouse Y") * ySpeed * dt;
+			 y = ClampAngle(y, yMinLimit, yMaxLimit);
+			 var rotation = Quaternion.Euler(y, x, 0f);
+			 var position = rotation * new Vector3(0.0f, 0.0f, -distance) + target.position;
+			 transform.rotation = rotation;
+			 transform.position = position;
+			 }
+		 }
+     
 	 
-     static float ClampAngle (float angle, float min, float max)
-     {
+     static float ClampAngle (float angle, float min, float max){
          if (angle < -360f)
              angle += 360f;
          if (angle > 360f)

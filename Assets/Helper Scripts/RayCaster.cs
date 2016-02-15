41c49f44542f75c4c16afdef4860bbbc673c8612
msLangdon95 <sameerasalameh95@gmail.com>
2016-02-15 10:22:17 -0800

    coloring, v0

--- Assets/Helper Scripts/RayCaster.cs
+++ Assets/Helper Scripts/RayCaster.cs
@@ -10,8 +10,29 @@ public class RayCaster : MonoBehaviour {
              ray = Camera.main.ScreenPointToRay(Input.mousePosition);
              if(Physics.Raycast(ray, out rayHit)){
                  lastClicked = rayHit.collider.gameObject;
-                 if(lastClicked != null)
-                     print(lastClicked.name);
+                 if(lastClicked != null){
+                    print(lastClicked.name);
+					if(OnClickChangeColor.flag==1){
+						if(OnClickChangeColor.myColor == 1){ //green
+							lastClicked.gameObject.GetComponent<Renderer>().materials[0].color = Color.green;
+							}
+						else if(OnClickChangeColor.myColor == 2){ //red
+							lastClicked.gameObject.GetComponent<Renderer>().materials[0].color =  Color.red;
+							}
+						else if(OnClickChangeColor.myColor == 3){ //blue
+							lastClicked.gameObject.GetComponent<Renderer>().materials[0].color =  Color.blue;
+							}
+						else if(OnClickChangeColor.myColor == 4){ //orange
+							lastClicked.gameObject.GetComponent<Renderer>().materials[0].color = new Color(255,165,0);
+							}
+						else if(OnClickChangeColor.myColor == 5){ //yellow
+							lastClicked.gameObject.GetComponent<Renderer>().materials[0].color =  Color.yellow;
+							}
+						else if(OnClickChangeColor.myColor == 6){ //white
+							lastClicked.gameObject.GetComponent<Renderer>().materials[0].color =  Color.white;
+							}
+						}
+				 }
              }
          }
      }

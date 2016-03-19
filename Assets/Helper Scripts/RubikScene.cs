28746dda66274ca7393f9d4cf83c872fb0a43dd5
msLangdon95 <sameerasalameh95@gmail.com>
2016-03-19 05:58:28 -0700

    miss danaw

--- Assets/Helper Scripts/RubikScene.cs
+++ Assets/Helper Scripts/RubikScene.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;    
 public class RubikScene : MonoBehaviour {
-	string [] UpperFace={"Corner3","Edge4","Corner8","Edge12","Corner6","Edge8","Corner2","Edge7"};
+	string [] UpperFace={"RED","Edge1","Edge3","Edge4","Edge12","Corner1","Corner2","Corner7","Corner8"};
 	/*string[] one = {"WHITE","Edge7","Edge8","Edge9","Edge12","Corner1","Corner4","Corner6","Corner7"};
 	string[] two = {"CENTER","ORANGE","RED","GREEN","BLUE","Edge1","Edge4","Edge6","Edge10"};
 	string[] three = {"YELLOW","Edge2","Edge3","Edge5","Edge11","Corner2","Corner3","Corner5","Corner8"};
@@ -13,24 +13,58 @@ public class RubikScene : MonoBehaviour {
 	string[] eight = {"Corner1","Corner2","Corner3","Corner4","Edge8","Edge6","Edge5","Edge4","GREEN"};
 	string[] nine = {"BLUE","Edge1","Edge2","Edge9","Edge10","Corner5","Corner6","Corner7","Corner8"};*/
 	public Vector3 p;
-	GameObject x,Parent,RubiksCube;
+	Vector2 fp,lp;
+	GameObject x,RubiksCube,Parent;
 	void Start(){
-		Parent=GameObject.Find("Parent");
 		RubiksCube=GameObject.Find("RubiksCube");
+		RubiksCube=GameObject.Find("Parent");
 		/*for(int i=0;i<48;i++){
 			x=GameObject.Find(Globals.EdgesAndCorners[i]);
 			x.GetComponent<Renderer>().material.color=Globals.CurrentCubeColors[i];
 		}*/
 	}
-	public void RotateUpper(Transform T){
-		for(int i=0;i<8;i++){
+	
+	void Update(){
+		if (Input.touchCount > 0){
+			Touch touch = Input.touches[0];
+			 if (touch.phase == TouchPhase.Began){
+                fp = touch.position;
+                lp = touch.position;
+          }
+		   if (touch.phase == TouchPhase.Moved ){
+                lp = touch.position;
+          }
+		   if(touch.phase == TouchPhase.Ended){ 
+                if((fp.x - lp.x) > 80) // left swipe
+				{
+					RotateUpper(Parent.transform,1);
+					//player.Rotate(0,-90,0);
+					//print("left");
+				}
+				else if((fp.x - lp.x) < -80) // right swipe
+				{
+					RotateUpper(Parent.transform,-1);
+					//player.Rotate(0,90,0);
+					//print("right");
+				}
+				else if((fp.y - lp.y) < -80 ) // up swipe
+				{
+				 print("up");
+				}
+			}
+		}
+	}
+	
+	
+	public void RotateUpper(Transform T,int flag){
+		for(int i=0;i<9;i++){
 			x=GameObject.Find(UpperFace[i]);
-			x.transform.SetParent(Parent.transform);
+			x.transform.SetParent(T);
 		}
 		p=GameObject.Find("Orange5").transform.position;
-		T.RotateAround(p, Vector3.up, 380 );
+		T.RotateAround(p, flag*Vector3.up, 380 );
 		
-		for(int i=0;i<8;i++){
+		for(int i=0;i<9;i++){
 			x=GameObject.Find(UpperFace[i]);
 			x.transform.SetParent(RubiksCube.transform);
 		}

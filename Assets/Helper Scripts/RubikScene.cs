2d1eb8f5fdd03018be7c187d4648dc6fad7d95b8
NoorSalman <noorsalman546@gmail.com>
2016-03-20 01:12:30 -0700

    time is finished ,cube is finished

--- Assets/Helper Scripts/RubikScene.cs
+++ Assets/Helper Scripts/RubikScene.cs
@@ -3,7 +3,15 @@ using System.Collections;
 using UnityEngine.UI;    
 public class RubikScene : MonoBehaviour {
 	string [] UpperFace={"Corner3","Edge4","Corner8","Edge12","Corner6","Edge8","Corner2","Edge7"};
-	
+	/*string[] one = {"WHITE","Edge7","Edge8","Edge9","Edge12","Corner1","Corner4","Corner6","Corner7"};
+	string[] two = {"CENTER","ORANGE","RED","GREEN","BLUE","Edge1","Edge4","Edge6","Edge10"};
+	string[] three = {"YELLOW","Edge2","Edge3","Edge5","Edge11","Corner2","Corner3","Corner5","Corner8"};
+	string[] four = {"ORANGE","Edge6","Edge7","Edge10","Edge11","Corner3","Corner7","Corner4","Corner5"};
+	string[] five = {"CENTER","Edge8","Edge9","Edge5","Edge2","WHITE","BLUE","GREEN","YELLOW","RED"};
+	string[] six = {"RED","Edge1","Edge3","Edge4","Edge12","Corner1","Corner2","Corner7","Corner8"};
+	string[] seven = {"CENTER","ORANGE","YELLOW","RED","WHITE","Edge3","Edge7","Edge11","Edge12"};
+	string[] eight = {"Corner1","Corner2","Corner3","Corner4","Edge8","Edge6","Edge5","Edge4","GREEN"};
+	string[] nine = {"BLUE","Edge1","Edge2","Edge9","Edge10","Corner5","Corner6","Corner7","Corner8"};*/
 	public Vector3 p;
 	GameObject x,Parent,RubiksCube;
 	void Start(){
---
+++ Assets/Helper Scripts/Timer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Timer : MonoBehaviour {
+
+	public float sec;
+	public float min;
+	public float hours;
+	public Text time; 
+	void Start(){
+	}
+	void Update () {
+		sec +=Time.deltaTime;
+		if(sec>=60){
+			min+=1;
+			sec=0;
+		}
+		if(min>=60){
+			hours+=1;
+			min=0;
+		}
+		time.text = hours + ":" + min + ":" + sec;
+	}
+
+}
+

3c05ba3bbe40445095105822198571ccbee71d68
NoorSalman <noorsalman546@gmail.com>
2016-04-26 11:01:32 -0700

    finished

---
+++ Assets/Helper Scripts/screenResolution.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class screenResolution : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Camera.main.aspect = 400f / 800f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

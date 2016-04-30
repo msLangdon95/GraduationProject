e0586178ca72c38f8274730e4a39bb0989acf862
msLangdon95 <sameerasalameh95@gmail.com>
2016-04-30 10:38:05 +0300

    yay!

--- Assets/Helper Scripts/RotateCamera.cs
+++ Assets/Helper Scripts/RotateCamera.cs
@@ -23,10 +23,19 @@
          // Make the rigid body not change rotation
             if (GetComponent<Rigidbody>())
              GetComponent<Rigidbody>().freezeRotation = true;
-		  transform.rotation =Quaternion.Euler(-1.2583486f,7.4292461f,-0.129597f);
-		 // transform.position =new Vector3(1689.247f, 932.0135f,-1224.659f);
+		 x=-50;
+		 y=20;
+		 DoIt();
      }
      
+	 void DoIt(){
+		 y = ClampAngle(y, -30f, 80f);
+		 var rotation = Quaternion.Euler(y, x, 0f);
+         var position = rotation * new Vector3(0.0f, 0.0f, -distance) + target.position;
+         transform.rotation = rotation;
+         transform.position = position;
+	 }
+	 
      void LateUpdate()
      {
 		 ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -36,20 +45,10 @@
 				return;
 				}
 			}
-         if (target != null && Input.GetMouseButton(0))
-         {
+         if (target != null && Input.GetMouseButton(0)){
              x += Input.GetAxis("Mouse X") * xSpeed * 0.04f;
              y -= Input.GetAxis("Mouse Y") * ySpeed * 0.04f;
-             
-             y = ClampAngle(y, -30f, 80f);
-             
-             var rotation = Quaternion.Euler(y, x, 0f);
-             var position = rotation * new Vector3(0.0f, 0.0f, -distance) + target.position;
-             
-             transform.rotation = rotation;
-			// print("rotation is "+ rotation.x+" "+rotation.y+" "+rotation.z);
-             transform.position = position;
-			// print("position is "+ position.x+" "+position.y+" "+position.z);
+             DoIt();
          }
      }
  
